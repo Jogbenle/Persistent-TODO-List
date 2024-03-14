61d75315862e2baba1cecfd212c6ae5cec6c0927
Gbenle John Olamide <gbenlejohn2rule@gmail.com>
2024-03-14 14:57:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let items = [];
+let items = ["Hello", "123"];
 
 const itemsDiv = document.getElementById("items")
 
@@ -8,23 +8,33 @@ function renderItems() {
 
     for (const [idx, item] of Object.entries(items)){
         const container = document.createElement("div")
-        container.style.maginBottom = "10px"
+        container.style.marginBottom = "10px"
 
         const text = document.createElement("p")
+        text.style.display ="inline"
+        text.style.marginRight = "10px"
         text.textContent = item;
 
         const button = document.createElement("button")
         button.textContent = "Delete"
+        button.onclick = () => removeItems(idx)
 
-        itemsDiv.appendChild(text)
+        container.appendChild(text)
+        container.appendChild(button)
+        
+
+        itemsDiv.appendChild(container)
     }
 }
+
 
 
 function loadItems() {}
 
 function saveItems() {}
 
-function addItem() {}
+function addItems() {}
 
-function removeItems() {}
+function removeItems(idx) {
+   
+}
